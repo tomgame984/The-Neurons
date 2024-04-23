850ab90aa95c9743633327b7f9c74bff3f316ec4
DeviloperG <frtglmz@gmail.com>
2024-04-23 12:09:53 +0100

    testy test

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+        <h1>I HAVE THE POWER!</h1>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
