886ea649e2846a80ddf3baab5c40d8d9345feeef
JackCollett <jack.collett@live.co.uk>
2024-04-25 12:11:26 +0100

    Gague added and resonsive to clicks of buttons

--- Frontend/src/components/Gague.jsx
+++ Frontend/src/components/Gague.jsx
@@ -1,10 +1,41 @@
 import { useState } from "react";
 import Buttons from './Buttons'
+import * as React from 'react';
+import { Gauge } from '@mui/x-charts/Gauge';
+import {
+  GaugeContainer,
+  GaugeValueArc,
+  GaugeReferenceArc,
+  useGaugeState,
+} from '@mui/x-charts/Gauge';
 
 
+function GaugePointer() {
+  const { valueAngle, outerRadius, cx, cy } = useGaugeState();
+
+  if (valueAngle === null) {
+    // No value to display
+    return null;
+  }
+
+  const target = {
+    x: cx + outerRadius * Math.sin(valueAngle),
+    y: cy - outerRadius * Math.cos(valueAngle),
+  };
+  return (
+    <g>
+      <circle cx={cx} cy={cy} r={5} fill="red" />
+      <path
+        d={`M ${cx} ${cy} L ${target.x} ${target.y}`}
+        stroke="red"
+        strokeWidth={3}
+      />
+    </g>
+  );
+}
 
 function Gague() {
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState(1)
 
     const resetCount = () => {
         setCount(0)
@@ -13,6 +44,20 @@ function Gague() {
     return (
       <>
         <h1>Energy Counter</h1>
+        <GaugeContainer
+      valueMin={-10}
+      valueMax={10}
+      width={400}
+      height={200}
+      startAngle={-70}
+      endAngle={70}
+      value={count}
+      >
+    
+      <GaugeReferenceArc />
+      <GaugeValueArc />
+      <GaugePointer />
+      </GaugeContainer>
         <div className="energy-Display">
           <p data-testid="counter">
             {count}
@@ -29,6 +74,6 @@ function Gague() {
         </div>
       </>
     )
-  };
+};
 
 export default Gague;
