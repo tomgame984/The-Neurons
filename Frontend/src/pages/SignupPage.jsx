dd3722b70ab13456811f21436f7fab99fe823eaf
Seyi-Toluhi <seyiadeyekun@gmail.com>
2024-04-25 14:27:01 +0100

    title added to signup page

--- Frontend/src/pages/SignupPage.jsx
+++ Frontend/src/pages/SignupPage.jsx
@@ -52,6 +52,7 @@ setNeurodiversity(event.target.value);
 
 return (
 <>
+    <title role="signup-page-title">Signup Page</title>
     <h2>Signup</h2>
     <form onSubmit={handleSubmit}>
     <label htmlFor="name">First Name:</label>
