cf2337be67ebcabf68076a1d470c51af40dd5e63
Seyi-Toluhi <seyiadeyekun@gmail.com>
2024-04-25 12:34:19 +0100

    q :wq

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './App.css'
 import { Dashboard } from './pages/Dashboard'
+import { HomePage } from "./pages/Landing/Homepage";
 
 
 const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
   {
     path: "/Dashboard",
     element: <Dashboard />,
