254b49c91ca9ac227828fdeca712c2d94169b1b5
Seyi-Toluhi <seyiadeyekun@gmail.com>
2024-04-26 09:16:08 +0100

    user model updated

--- api/models/user.js
+++ api/models/user.js
@@ -5,8 +5,8 @@ const UserSchema = new mongoose.Schema({
   surname: { type: String },
   email: { type: String },
   password: { type: String },
-  score: { type: Number },
   neurodiversity: { type: String },
+  score: { type: Number },
   event_history: { type: Object, required: false }
 });
 
