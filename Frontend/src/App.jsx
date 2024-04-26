66a1b237f3d60c6637e9d276cc7e13de8ffbb4ca
Seyi-Toluhi <seyiadeyekun@gmail.com>
2024-04-26 11:07:02 +0100

    route added to app.js for signup page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './App.css'
 import { Dashboard } from './pages/Dashboard'
 import { HomePage } from "./pages/Landing/Homepage";
+import { SignupPage } from "./pages/SignupPage"
 
 
 const router = createBrowserRouter([
@@ -9,6 +10,10 @@ const router = createBrowserRouter([
     path: "/",
     element: <HomePage />,
   },
+  {
+    path: "/signup",
+    element: <SignupPage/>,
+  }
   {
     path: "/Dashboard",
     element: <Dashboard />,
