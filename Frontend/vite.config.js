a03ecef767ce56366c23c524f1d3b89d6010f27e
DeviloperG <frtglmz@gmail.com>
2024-04-30 10:08:43 +0100

    test coverage working

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,6 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   test: {
+    coverage: {
+      enabled: true,
+      reporter: ["text", "html"]
+    },
+    
     globals: true,
     environment: "jsdom",
   },
