e0ab37042b87fd2f9aede276c8abaa2f5dc6a32b
KwibooReese <reesehayman@gmail.com>
2024-04-30 17:29:51 +0100

    Tests described

---
+++ Frontend/tests/components/ActivityForm-integration.test.jsx
@@ -0,0 +1,14 @@
+// When a positive score between 1 and 3 is inputted the counter is updated correctly
+
+
+// When a negative score between 1 and 3 is inputted the counter is updated correctly
+
+
+// When an incorrect data type e.g. string is inputted to score and submit is clicked,
+// submit is not allowed
+
+
+// When no data is inputted, form cannot submit and an error is shown and form does not dissapear
+
+
+// When a string is inputted in the description, the string is stored in state
---
+++ Frontend/tests/components/ActivityForm.test.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// When button icon is clicked ActivityForm renders
+
+
+
+// When the close button icon is clicked the form disappears
+
+
+
+// When submit button is clicked and score is filled, ActivityForm dissapears
+
+
+
