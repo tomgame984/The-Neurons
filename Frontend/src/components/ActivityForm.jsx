e9b9ed07f5cbf3948a0d388aee6af0553ef87eb3
Tom Game <139250181+tomgame984@users.noreply.github.com>
2024-05-01 10:56:27 +0100

    Update ActivityForm.jsx
    Jack told me to

--- Frontend/src/components/ActivityForm.jsx
+++ Frontend/src/components/ActivityForm.jsx
@@ -43,13 +43,9 @@ const ActivityForm = (props) => {
             id="outlined-basic"
             label="Score"
             variant="outlined"
-<<<<<<< HEAD
-            onChange={(event) => {
-                handleNumber(parseInt(event.target.value));
-            }}
-=======
+
             onChange={(event) => {handleNumber(parseInt(event.target.value))}}
->>>>>>> a815ff8a816c9213f964457f4e1f27e43de2b630
+              
           ></TextField>
           <Button
             variant="contained"
