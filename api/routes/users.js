71e7290699c03f448e295206e02d8092c2042bbe
DeviloperG <frtglmz@gmail.com>
2024-05-01 16:04:43 +0100

    connected routes to frontend to display event history upon submit button trigger & pulling user_score to local storage

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,7 +5,7 @@ const UsersController = require("../controllers/users");
 const tokenChecker = require("../middleware/tokenChecker");
 
 router.post("/", UsersController.create); //used to log existing users into the system
-router.get("/", tokenChecker, UsersController.getUser); //used to create new
+router.get("/", tokenChecker, UsersController.getUser); //
 router.put("/", tokenChecker, UsersController.updateUserEventHistory); //update User Event History
 
 
