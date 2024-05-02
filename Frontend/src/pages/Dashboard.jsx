71b901a3a2db47d92481110ad6859f381b86ce2a
Seyi-Toluhi <seyiadeyekun@gmail.com>
2024-05-02 16:03:35 +0100

    eventscore removed from dashboard

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -77,7 +77,6 @@ export const Dashboard = () => {
           <div key={eventId} style={{ border: "1px solid black", padding: "10px", marginBottom: "10px" }}>
             <p role="event-category">Category: {events[eventId].category}</p>
             <p role="event-description">Description: {events[eventId].description}</p>
-            <p role="event-score">Event Score: {events[eventId].eventScore}</p>
           </div>
         ))}
       </div>
